e11da5492b29de64dd0147cddb70ee21ed6df5f5
Sanjeev Yadav <85263808+Sb-123@users.noreply.github.com>
2024-08-31 11:31:12 +0000

    working on the 02counter project

--- 01viteReact_sb/src/main.jsx
+++ 01viteReact_sb/src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App.jsx'
+import App from "./App.jsx";
 
-function MyApp(){
-    return (
-        <div>
-            <h1>Custom App | chai</h1>
-        </div>
-    )
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | chai</h1>
+    </div>
+  );
 }
 
 // const ReactElement = {
@@ -21,22 +21,18 @@ function MyApp(){
 // }
 
 const anotherElement = (
-    <a href="https://google.com" target='_blank'>Visit google</a>
-)
+  <a href="https://google.com" target="_blank">
+    Visit google
+  </a>
+);
 
-
-
-const anotherUser = "chai aur react"
+const anotherUser = "chai aur react";
 
 const reactElement = React.createElement(
-    'a',
-    {href: 'https://google.com',target: '_blank' },
-    'click me to visit google',
-    anotherElement
-)
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click me to visit google",
+  anotherElement
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- 
-    reactElement
-  
-)
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
